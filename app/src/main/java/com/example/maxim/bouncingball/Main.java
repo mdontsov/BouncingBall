d36a216c438e5152d575bea92ca4243160a5682b
mdontsov <maksim.dontsov@gmail.com>
2016-01-06 21:07:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/maxim/bouncingball/Main.java
@@ -0,0 +1,21 @@
+package com.example.maxim.bouncingball;
+
+
+import android.os.Bundle;
+
+import android.app.Activity;
+
+
+public class Main extends Activity {
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+    }
+
+}
